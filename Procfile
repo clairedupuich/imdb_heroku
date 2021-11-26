e18655cb110199fc57a66b4bc610a03a5efb7f58
claire <1097457668@qq.com>
2021-11-26 09:24:36 +0100

    test3

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-
 web: sh setup.sh && streamlit run streamlit_imdb.py
-
--- setup.sh
+++ setup.sh
@@ -1,19 +1,9 @@
-#配置cd
 mkdir -p ~/.streamlit/
 
-
-
-
 echo "\
-
 [server]\n\
-
 headless = true\n\
-
 port = $PORT\n\
-
-ennbleCORS = false\n\
-
+enableCORS = false\n\
 \n\
-
 " > ~/.streamlit/config.toml
